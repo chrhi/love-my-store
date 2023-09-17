3a43f95a052342b19ad2c65aa39f2a7d62afa5b7
abdellah <Abdellah.chehri14@gmail.com>
2023-09-17 09:54:52 +0200

    add

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,10 +1,50 @@
 import { MainNav } from "@/components/layout/main-nav";
 import { UserNav } from "@/components/user-nav";
 
-export default function AppLayout({ children }: { children: React.ReactNode }) {
+import { Metadata } from "next";
+
+import { Separator } from "@/components/ui/separator";
+import { SidebarNav } from "@/components/sidebar-nav";
+
+export const metadata: Metadata = {
+  title: "shope dz",
+  description: "Advanced form example using react-hook-form and Zod.",
+};
+
+const items = [
+  { name: "overview", path: "/admin" },
+  { name: "Customers", path: "/admin/customers" },
+  { name: "Products", path: "/admin/products" },
+  { name: "Settings", path: "/admin/settings" },
+];
+
+const sidebarNavItems = [
+  {
+    title: "overview",
+    href: "/admin",
+  },
+  {
+    title: "Customers",
+    href: "/admin/customers",
+  },
+  {
+    title: "Products",
+    href: "/admin/products",
+  },
+  {
+    title: "Settings",
+    href: "/admin/settings",
+  },
+];
+
+interface SettingsLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function AppLayout({ children }: SettingsLayoutProps) {
   return (
-    <div className="w-full h-fit ">
-      <div className="border-b fixed top-0 z-99 flex justify-center  w-full h-16 bg-white ">
+    <>
+      <div className="border-b fixed top-0 z-99 flex justify-center  w-full h-16 bg-black ">
         <div className="flex container mx-auto h-16 justify-between items-center px-4">
           <MainNav />
           <div>
@@ -12,7 +52,23 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
           </div>
         </div>
       </div>
-      <div className="container mx-auto ">{children}</div>
-    </div>
+      <div className="  space-y-6 p-10 pb-16 block">
+        <div className="space-y-0.5">
+          <h2 className="text-2xl font-bold tracking-tight">
+            Organization settings
+          </h2>
+          <p className="text-muted-foreground">
+            Manage your account settings and set e-mail preferences.
+          </p>
+        </div>
+        <Separator className="my-6" />
+        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+          <aside className="-mx-4 lg:w-1/5">
+            <SidebarNav items={sidebarNavItems} />
+          </aside>
+          <div className="flex-1 lg:max-w-2xl">{children}</div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,18 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
 
-export const config = { matcher: ["/admin/:path*"] };
+// export const config = { matcher: ["/admin/:path*"] };
 
 // export default function middleware() {
 //   //todo handle that later
 // }
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  callbacks: {
+    authorized({ req, token }) {
+      return !!token;
+    },
+  },
+});
+
+export const config = { matcher: ["/admin/:path*"] };
